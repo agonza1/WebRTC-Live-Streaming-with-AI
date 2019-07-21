729f298ac6566b07c6a9bb68af08ab72f594fed0
agonza1 <albertogontras@gmail.com>
2019-07-21 12:54:35 -0500

    working receive, modify and send RTP stream

--- video-ai-service/index.js
+++ video-ai-service/index.js
@@ -4,8 +4,8 @@ async function main() {
   console.log(cv.getBuildInformation());
   let frame;
   // const wCap = new cv.VideoCapture(0);
-  const vCap = new cv.VideoCapture('videotestsrc ! videoconvert ! video/x-raw,width=640,height=480 ! appsink')
-  const w = new cv.VideoWriter('appsrc ! videoparse width=640 height=480 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=8004', 0, 25, new cv.Size(640, 480));
+  const vCap = new cv.VideoCapture('videotestsrc ! video/x-raw,format=BGR,width=640,height=480,framerate=25/1 ! appsink')
+  const w = new cv.VideoWriter('appsrc ! videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=25/1 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=8004', 0, 25, new cv.Size(640, 480));
   const intvl = setInterval(() => {
     frame = vCap.read();
     // loop back to start on end of stream reached
@@ -24,7 +24,7 @@ async function main() {
     frame.release();
     // w.release();
 
-  }, 100);
+  }, 40);
 
   // w.release();
   //child.stdout.pipe(res);
--- video-ai-service/testing.js
+++ video-ai-service/testing.js
@@ -4,7 +4,7 @@ const cv = require('opencv4nodejs');
 //const vCap = new cv.VideoCapture('./video_test.mp4');
 //const vCap = new cv.VideoCapture('udpsrc port=5000 ! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert ! appsink name=appsink0 emit-signals$
 //const vCap = new cv.VideoCapture('videotestsrc pattern=snow ! video/x-raw,width=1280,height=720 ! appsink name=appsink0')
-const vCap = new cv.VideoCapture('udpsrc port=5000 ! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert ! appsink location=/dev/stdout');
+const vCap = new cv.VideoCapture('udpsrc port=5000 ! application/x-rtp,payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! decodebin ! videoconvert ! appsink location=/dev/stdout');
 
 // read frames from capture
 //const frame = vCap.read();
@@ -15,12 +15,13 @@ const vCap = new cv.VideoCapture('udpsrc port=5000 ! application/x-rtp, payload=
 // loop through the capture
 const delay = 10;
 let done = false;
+let fourcc = vCap.get(6);
 //const w = new cv.VideoWriter('appsrc ! videoconvert ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink port=8004', 0x00000021, 25, new cv.Size(640, 480))
 //const w = new cv.VideoWriter('appsrc name=appsink0 !  videoconvert ! x264enc ! rtph264pay ! udpsink host=3.91.99.131 port=8004', 0, 25, new cv.Size(1280, 720))
 //multiple new streams
 //const w = new cv.VideoWriter('appsrc ! videoconvert ! x264enc ! mpegtsmux ! udpsink host=127.0.0.1 port=8004', 0, 25, new cv.Size(1280, 720))
 //const w = new cv.VideoWriter('videocap.mp4', 0x00000021, 25, new cv.Size(640, 480))
-const w = new cv.VideoWriter('appsrc ! videoparse width=640 height=480 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=8004', 0, 25, new cv.Size(640, 480))
+const w = new cv.VideoWriter('appsrc ! videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=25/1 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=8004', 0, 25, new cv.Size(640, 480));
 while (!done) {
     let frame = vCap.read();
     // loop back to start on end of stream reached
@@ -29,12 +30,12 @@ while (!done) {
         frame = vCap.read();
     }
     console.log(frame);
-
-    frame.bgrToGray();
+    let pFrame = cv.applyColorMap(frame, cv.COLORMAP_AUTUMN);
+    // let bwframe = frame.bgrToGray();
 
     console.error('will write frame');
-    w.write(frame);
+    w.write(pFrame);
     console.error('has written frame');
-    //const key = cv.waitKey(delay);
-    //done = key !== 255;
+    const key = cv.waitKey(delay);
+    done = key !== -1 && key !== 255;
 }
